d4dc4bfa2691bed7513d94b2974caff372507b83
WarmHatori <warmhatori@gmail.com>
2023-11-15 22:37:14 +0700

    Volunteer & Sponsor

---
+++ website-covenant/resources/views/admin/editsponsor.blade.php
@@ -0,0 +1,19 @@
+@if($errors)
+    <ul>
+        @foreach($errors->all() as $error)
+            <li>{{$error}}</li>
+        @endforeach
+    </ul>
+@endif
+
+<form action="/sponsor/{{$sponsor->id}}" method="post">
+    @csrf
+    Instansi <input type="text" name="instansi" value="{{$sponsor->instansi}}" /><br />
+    Penanggung Jawab <input type="text" name="penanggung_jawab" value="{{$sponsor->penanggung_jawab}}" /><br />
+    <button type="submit">SUBMIT</button>
+</form>
+<form action="/sponsor/{{$sponsor->id}}" method="post">
+    @method('DELETE')
+    @csrf
+    <button type="submit">DELETE</button>
+</form>
---
+++ website-covenant/resources/views/admin/sponsor.blade.php
@@ -0,0 +1,36 @@
+@if($errors)
+    <ul>
+        @foreach($errors->all() as $error)
+            <li>{{$error}}</li>
+        @endforeach
+    </ul>
+@endif
+
+<div>
+    <form action="/sponsor" method="post" enctype="multipart/form-data"> 
+        @csrf 
+        <h2>UPLOAD DATA SPONSOR</h2>
+        <div><input type="text" name="instansi"placeholder="Instansi" /></div>
+        <div><input type="text" name="penanggung_jawab" placeholder="Penanggung Jawab" /></div>
+        <button type="submit">Submit</button>
+        </form>
+</div>
+
+<table border="1"> 
+    <tr> 
+        <th>No.</th>
+        <th>Instansi</th>
+        <th>Penanggung Jawab</th>
+        </tr>
+        @foreach($sponsors as $sponsor)
+        <tr>
+            <th>{{$sponsor->id}}</th>
+            <th>{{$sponsor->instansi}}</th>
+            <th>{{$sponsor->penanggung_jawab}}</th>
+            <td>
+                <a href="/sponsor/{{$sponsor->id}}/show">EDIT</a>
+            </td>
+        </tr>
+        @endforeach
+
+</table>
