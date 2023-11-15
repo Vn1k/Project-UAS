612bff64999a5c94637c7c63ba88cd6df5d056f1
acd <127939247+acd17@users.noreply.github.com>
2023-11-15 23:53:27 +0700

    up css

--- website-covenant/resources/views/admin/kegiatan.blade.php
+++ website-covenant/resources/views/admin/kegiatan.blade.php
@@ -4,13 +4,13 @@
     @vite(['resources/css/app.css','resources/js/app.js'])
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Kegiatan</title>
 </head>
 <body>
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="containerKegiatan">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -24,44 +24,44 @@
 <h1>Kegiatan</h1>
 <form action="/kegiatans" method="post" enctype="multipart/form-data">
     @csrf
-    <a class="block items-center justify-center w-auto m-6 p-6 bg-white border border-gray-200 rounded-lg shadow ">
+    <a class="cardForm block m-10 p-6 bg-white border border-gray-200 rounded-lg shadow ">
         <div class="grid gap-6 mb-6 md:grid-cols-2">
             <div>
-                <label for="nama_kegiatan" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
-                <input type="text" id="nama_kegiatan" name="nama_kegiatan" class=" kegiatan max-w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nama Kegiatan" required>
+                <label for="nama_kegiatan" class=" block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
+                <input type="text" id="nama_kegiatan" name="nama_kegiatan" class="formKegiatan kegiatan bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nama Kegiatan" required>
             </div>
         </div>
-        <div class="containerTWO flex">
-            <div class="mb-6 md:mr-6 w-auto">
+        <div class="containerTWO flex mb-10">
+            <div class=" mb-6 md:mr-6 w-auto">
                 <label for="tanggal" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
-                <input type="date" id="tanggal" name="tanggal" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                <input type="date" id="tanggal" name="tanggal" class="formTanggal bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
             </div>
 
             <div class="mb-6 md:mr-6">
                 <label for="waktu" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
-                <input type="time" id="waktu" name="waktu" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                <input type="time" id="waktu" name="waktu" class="formWaktu bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
             </div>
 
             <div class="grid gap-6 mb-6 md:grid-cols-2">
                 <div>
                     <label for="penyelenggara" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
-                    <input type="text" id="penyelenggara" name="penyelenggara" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Oleh" required>
+                    <input type="text" id="penyelenggara" name="penyelenggara" class="formOleh bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Oleh" required>
                 </div>
             </div>
 
         </div>
-        <div class="containerLD flex">
+        <div class="containerLD flex mb-10">
             <div class="grid mb-6 md:grid-cols-2">
                 <div>
-                    <label for="lokaso" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
-                    <input type="text" id="lokasi" name="lokasi" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Lokasi" required>
+                    <label for="lokasi" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
+                    <input type="text" id="lokasi" name="lokasi" class="formLokasi bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Lokasi" required>
                 </div>
             </div>
 
             <div class="grid mb-6 md:grid-cols-2">
                 <div>
                     <label for="deskripsi" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
-                    <input type="text" id="deskripsi" name="deskripsi" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Deskripsi" required>
+                    <input type="text" id="deskripsi" name="deskripsi" class="formDesc bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Deskripsi" required>
                 </div>
             </div>
         </div>
@@ -69,12 +69,12 @@
 
         <!-- Add other fields similar to the example provided -->
 
-        <button type="submit" class=" items-center justify-center text-black bg-yellow-100 hover:bg-yellow-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+        <button type="submit" class="btnFormKegiatan items-center justify-center text-black bg-yellow-100 hover:bg-yellow-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
     </a>
 </form>
 
             
-            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <div class="tableKegiatan overflow-x-auto shadow-md sm:rounded-lg">
                 <table class="w-full text-sm text-left rtl:text-right text-blue-100 dark:text-blue-100">
                     <thead class="text-xs text-black uppercase bg-yellow-100 dark:text-white">
                         <tr>
