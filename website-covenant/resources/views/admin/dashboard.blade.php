bcf05f1740f70dd0ce2bea2addf1ea68c29c4cc6
Vn1k <56496329+Vn1k@users.noreply.github.com>
2023-11-21 03:16:47 +0700

    Update dashboard.blade.php

--- website-covenant/resources/views/admin/dashboard.blade.php
+++ website-covenant/resources/views/admin/dashboard.blade.php
@@ -1,1 +1,8 @@
 <h1>dashboard admin</h1>
+
+<form id="logout-form" action="{{ route('admin.logout') }}" method="POST" style="display: none;">
+        @csrf
+    </form>
+    <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+        Logout
+    </a>
