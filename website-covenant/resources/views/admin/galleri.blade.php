4c976797803d91c05d0aa6dff3e20ef7e183bf99
acd <127939247+acd17@users.noreply.github.com>
2023-11-21 00:02:00 +0700

    80% gallery

--- website-covenant/resources/views/admin/galleri.blade.php
+++ website-covenant/resources/views/admin/galleri.blade.php
@@ -5,6 +5,8 @@
     @vite(['resources/css/app.css','resources/js/app.js'])
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/fontawesome.min.css" />  
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css" />
     <title>Document</title>
 </head>
 
@@ -13,61 +15,84 @@
         <div class="col-md-6">
             <div class="card">
                 <div class="card-body">
-                    <form class="w-px-500 p-3 p-md-3" action="{{ route('galleri.store') }}" method="post" enctype="multipart/form-data">
+                <div class="card">
+                <div class="titlegalleri card-header mt-10">Upload Galleri</div>
+                <!-- <div class="card-body">
+                    <p>You are in Kegiatan Page</p>
+                </div> -->
+            </div>
+            <form class="formGAL w-px-500 p-3 p-md-3" action="{{ route('galleri.store') }}" method="post" enctype="multipart/form-data">
+                <a class="cardGAL block m-10 p-10 bg-white border border-gray-200 rounded-lg shadow">
+                    
                         @csrf
-                        <a class="cardForm block m-10 p-6 bg-white border border-gray-200 rounded-lg shadow ">
-                                <div class="col-sm-9 items-center justify-center">
-                                    <label class="block mb-2 text-sm items-center justify-center font-medium text-gray-900 dark:text-white" for="user_avatar">Upload file</label>
-                                    <input class="block w-80 items-center justify-center text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="user_avatar_help" id="user_avatar" type="file" name="image" @error('image') is-invalid @enderror>
-                                    @error('image')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
+                        
+                            <div class="containerGalleri">
+                                    <div class="fileGAL col-sm-9 items-center justify-center">
+                                        <label class="nameGAL block mb-2 text-sm items-center justify-center font-medium text-gray-900 dark:text-white" for="user_avatar">Upload file</label>
+                                        <input class="block w-80 items-center justify-center text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="user_avatar_help" id="user_avatar" type="file" name="image" @error('image') is-invalid @enderror>
+                                        @error('image')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
+                                    </div>
                                 </div>
-                            </div>
 
 
-                            <div class="row mb-3">
-                                <label class="col-sm-3 col-form-label"></label>
-                                <div class="col-sm-9">
-                                    <button type="submit" class="btnFormKegiatan items-center justify-center text-black bg-yellow-100 hover:bg-yellow-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                                <div class="row mb-3">
+                                    <label class="col-sm-3 col-form-label"></label>
+                                    <div class="col-sm-9">
+                                        <button type="submit" class="btnFormGalleri items-center justify-center text-black bg-yellow-100 hover:bg-yellow-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                                    </div>
                                 </div>
                             </div>
-                        </a>
+                                
+                       </a> 
                     </form>
+                
                 </div>
             </div>
         </div>
     </div>
-    <div class="card-body">
-        <table class="table-responsive" style="width: 100%">
-            <thead>
-                <th>Images</th>
-                <th>Action</th> <!-- Add a column for the action -->
-            </thead>
-            <tbody>
-                @foreach($image as $item)
-                <tr>
-                    <td>
-                        @if($item->image)
-                        <img src="{{ asset('storage/images/'.$item->image) }}" style="height: 500px; width: 500px;">
-                        </td>
-                        <td>
-                            <form action="{{ route('galleri.destroy', ['galleri' => $item->id]) }}" method="POST" style="display: inline;">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" style="border: none; background-color: transparent; color: red; text-decoration: underline; cursor: pointer;">Delete</button>
-                            </form>
-                        </td>
-                        @else
-                        <span>No image found!</span>
-                        @endif
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+    <div class="tableGal relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="contGAL text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr class="titleGal">
+                <th scope="col" class="px-6 py-3">
+                    Images
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Action
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($image as $item)
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <td class="inputIMG px-6 py-4">
+                    @if($item->image)
+                    <img src="{{ asset('storage/images/'.$item->image) }}" style="height: 100px; width: 100px;">
+                    @else
+                    <span>No image found!</span>
+                    @endif
+                </td>
+                <td class="logoBIN px-6 py-4 text-right">
+                    @if($item->image)
+                    <form action="{{ route('galleri.destroy', ['galleri' => $item->id]) }}" method="POST" style="display: inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" >
+                            <i class="fa-solid fa-trash"></i>
+                        </button>
+                    </form>
+                    @endif
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
 </body>
 
 </html>
