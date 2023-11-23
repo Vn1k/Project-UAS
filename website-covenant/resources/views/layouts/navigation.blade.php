2724d7f001ffd5ed58cd4974827b876a4efd19d3
Vinik <56496329+Vn1k@users.noreply.github.com>
2023-11-23 22:11:25 +0700

    Update navigation.blade.php

--- website-covenant/resources/views/layouts/navigation.blade.php
+++ website-covenant/resources/views/layouts/navigation.blade.php
@@ -15,32 +15,17 @@
             <div class="flex justify-between h-16">
                 <!-- Settings Dropdown -->
                 <div class="hidden sm:flex sm:items-center sm:ms-6">
-                    <x-dropdown align="right" width="48">
-                        <x-slot name="trigger">
-                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                                <div>{{ Auth::user()->name }}</div>
-
-                                <div class="ms-1">
-                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                    </svg>
-                                </div>
-                            </button>
-                        </x-slot>
-
-                        <x-slot name="content">
-                            <!-- Authentication -->
-                            <form method="POST" action="{{ route('admin.logout') }}">
-                                @csrf
-                                <x-dropdown-link :href="route('admin.logout')" onclick="event.preventDefault();
-                                    this.closest('form').submit();">
-                                    {{ __('Log Out') }}
-                                </x-dropdown-link>
-                            </form>
-                        </x-slot>
-                    </x-dropdown>
+                    <div>
+                        <a href="{{ route('admin.dashboard') }}">{{ Auth::user()->name }}</a>
+                    </div>
                 </div>
-
+                <form method="POST" action="{{ route('admin.logout') }}" class="hidden sm:flex sm:items-center sm:ms-6">
+                    @csrf
+                    <x-dropdown-link :href="route('admin.logout')" onclick="event.preventDefault();
+                                    this.closest('form').submit();">
+                        {{ __('Log Out') }}
+                    </x-dropdown-link>
+                </form>
                 <div class="hidden sm:flex sm:items-center sm:ms-6">
                     <a href="{{ route('galleri.index') }}" class="text-gray-500 hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">Galleri</a>
                 </div>
