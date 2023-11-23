5d2c564b219f2949ff72edc0e69109f4b7e083a3
Vinik <56496329+Vn1k@users.noreply.github.com>
2023-11-23 22:46:05 +0700

    Update guest.blade.php

--- website-covenant/resources/views/layouts/guest.blade.php
+++ website-covenant/resources/views/layouts/guest.blade.php
@@ -16,7 +16,7 @@
     </head>
     <body>
         <div>
-            <div>
+            <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
                 {{ $slot }}
             </div>
         </div>
