d0dd4ba27093ce6f3793c8dda2bd12864dae3978
Vinik <56496329+Vn1k@users.noreply.github.com>
2023-11-28 00:48:35 +0700

    about-us

--- website-covenant/resources/views/front/tentangKami.blade.php
+++ website-covenant/resources/views/front/tentangKami.blade.php
@@ -11,7 +11,7 @@
   <section class="bg-white">
     <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
       <div class="grid grid-cols-1 gap-4 mt-8">
-        <img class="w-full rounded-lg" src="{{ asset('storage/images/gambar-pertama.png') }}" alt="Logo" style="max-width: 400px; /* set your desired max width */">
+        <img class="w-full rounded-lg" src="{{ asset('asset/logo-covenant.svg') }}" alt="Logo" style="max-width: 400px; /* set your desired max width */">
       </div>
       <div class="font text-gray-500 sm:text-lg dark:text-gray-400">
         <h2 class="mb-4 text-4xl tracking-tight text-gray-900 dark:text-black">Sejarah Rumah Belajar Covenant</h2>
@@ -26,7 +26,7 @@
         <p class="mb-4 text-black">Namun seiring berjalannya waktu ternyata anak-anak jalanan tersebut hanya sebagai permulaan. Karena hanya dalam beberapa bulan saja mereka ingin belajar, mereka sudah mulai malas datang ke sekolah kami dan lebih memilih mencari uang dan bermain game dari pada bersekolah dan belajar.</p>
       </div>
       <div class="grid grid-cols-1 gap-4 mt-8">
-        <img class="w-full rounded-lg" src="{{ asset('storage/images/gambar-kedua.png') }}" alt="Logo" style="max-width: 840px; /* set your desired max width */">
+        <img class="w-full rounded-lg" src="{{ asset('asset/section-2-about-us.svg') }}" alt="Logo" style="max-width: 840px; /* set your desired max width */">
       </div>
     </div>
   </section>
@@ -34,7 +34,7 @@
   <section class="bg-white">
     <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
       <div class="grid grid-cols-1 gap-4 mt-8">
-        <img class="w-full rounded-lg" src="{{ asset('storage/images/gambar-ketiga.png') }}" alt="Logo" style="max-width: 840px; /* set your desired max width */">
+        <img class="w-full rounded-lg" src="{{ asset('asset/section-3-about-us.svg') }}" alt="Logo" style="max-width: 840px; /* set your desired max width */">
       </div>
       <div class=" text-gray-500 sm:text-lg dark:text-gray-400">
         <p class="mb-4 text-black">Dalam proses belajar mengajar pun anak-anak Kami disedikan tempat untuk belajar sebanyak 3 kontrakan. Kami mengontrak tanpa adanya donatur yang membantu kami. Dengan begitu kami mengumpulkan uang dengan cara mengamen dan juga berjualan. setiap guru yang mengajar di Paud Covenant selalu mengamen setiap malam. Hasil yang kami dapatkan dalam satu malam sebanyak 100-300 Ribu dan kami gunakan untuk membayar kontrakan dan juga biaya transportasi yang kami gunakan untuk menjemput anak-anak kami.</p>
@@ -50,7 +50,7 @@
         <p class="mb-4 text-black rounded-md shadow-md p-2 bg-white">Membuat sekolah tempat anak-anak marginal sekolah</p>
       </div>
       <div class="grid grid-cols-1 gap-4 mt-8">
-        <img class="w-full rounded-lg" src="{{ asset('storage/images/gambar-keempat.png') }}" alt="Logo" style="max-width: 840px; /* set your desired max width */">
+        <img class="w-full rounded-lg" src="{{ asset('asset/gambar-with-baloon.svg') }}" alt="Logo" style="max-width: 840px; /* set your desired max width */">
       </div>
     </div>
   </section>
