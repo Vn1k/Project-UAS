61ced726a06f9a8511fbb6635b12e290fb19a921
Vinik <56496329+Vn1k@users.noreply.github.com>
2023-12-01 20:36:02 +0700

    Update dashboard.blade.php

--- website-covenant/resources/views/admin/dashboard.blade.php
+++ website-covenant/resources/views/admin/dashboard.blade.php
@@ -12,11 +12,39 @@
     @section('content')
     <div class="p-4 sm:ml-64">
         <div class="p-4">
-            <h3>Sponsor count: {{ $countspon }} </h3>
-            <h3>Volunteer Count {{$countvoult}}</h3>
-            <h3>Kegiatan {{$countkegiatan}}</h3>
-            <h3>galleri {{$countgaleri}}</h3>
-            <h3>supporter {{$countsupp}}</h3>
+            <div class="mb-5 text-4xl text-center font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl dark:text-white">Welcome</div>
+            <div class="grid md:grid-cols-5 md:gap-5 font-medium font-kanit">
+                <a class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <div class="flex flex-col items-center justify-center">
+                        <dt class="mb-2 text-3xl font-extrabold">{{ $countspon }}</dt>
+                        <dd class="text-gray-500 dark:text-gray-400">Sponsor</dd>
+                    </div>
+                </a>
+                <a class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <div class="flex flex-col items-center justify-center">
+                        <dt class="mb-2 text-3xl font-extrabold">{{$countvoult}}</dt>
+                        <dd class="text-gray-500 dark:text-gray-400">Volunteer</dd>
+                    </div>
+                </a>
+                <a class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <div class="flex flex-col items-center justify-center">
+                        <dt class="mb-2 text-3xl font-extrabold">{{$countkegiatan}}</dt>
+                        <dd class="text-gray-500 dark:text-gray-400">Kegiatan</dd>
+                    </div>
+                </a>
+                <a class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <div class="flex flex-col items-center justify-center">
+                        <dt class="mb-2 text-3xl font-extrabold">{{$countgaleri}}</dt>
+                        <dd class="text-gray-500 dark:text-gray-400">Galleri</dd>
+                    </div>
+                </a>
+                <a class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <div class="flex flex-col items-center justify-center">
+                        <dt class="mb-2 text-3xl font-extrabold">{{$countsupp}}</dt>
+                        <dd class="text-gray-500 dark:text-gray-400">Supporter</dd>
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
     @endsection
