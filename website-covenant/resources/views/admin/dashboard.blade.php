899e2bbb508b9b2d1629e9222a92964943c1ebbf
Vinik <56496329+Vn1k@users.noreply.github.com>
2023-12-01 18:48:18 +0700

    dashboard

--- website-covenant/resources/views/admin/dashboard.blade.php
+++ website-covenant/resources/views/admin/dashboard.blade.php
@@ -1,5 +1,21 @@
-@extends('layouts.navigation')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<h1>dashboard admin</h1>
-@endsection
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+
+<body>
+    @extends('layouts.navigation')
+    @section('content')
+    <div class="p-4 sm:ml-64">
+        <div class="p-4">
+            <h2>Total Sponsors: {{ $totalsponsor }}</h2>
+        </div>
+    </div>
+    @endsection
+</body>
+
+</html>
--- website-covenant/resources/views/admin/supporter.blade.php
+++ website-covenant/resources/views/admin/supporter.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Kanit:wght@400&display=swap">
-    <title>Admin List Supporter</title>
+    <title>Supporter</title>
     <style>
         .popup-overlay {
         display: none;
