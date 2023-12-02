91e6c2308aaa2c96f4c274432e54a173510197fa
Vinik <56496329+Vn1k@users.noreply.github.com>
2023-12-02 22:16:26 +0700

    new table biar 255

--- website-covenant/database/migrations/2023_12_02_221520_create_supporters_table.php
+++ website-covenant/database/migrations/2023_12_02_221520_create_supporters_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('supporters', function (Blueprint $table) {
             $table->id();
             $table->string('nama', 50);
-            $table->string('email',50);
+            $table->string('email');
             $table->string('alamat',255);
             $table->string('no_telepon', 13);
             $table->enum('donasi', ['uang_tunai', 'barang']);
